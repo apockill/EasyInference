ace24744c8f75f93a4d0c4030f03579e9cf3c8fa
Alex Thiel <Alex.D.Thiel@gmail.com>
2018-05-03 18:19:54 -0800

    Updated files after finishing the Hannover Messe demo

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
     version='0.1',
     description='This is a helper library for me to write simple easyinference wrappers over pretrained TF models, that '
                 'require none of the original model definition code. ',
-    packages=['easyinference'],
+    packages=['easyinference',
+              'pyserial'],
     install_requires=[],
     zip_safe=False
 )
