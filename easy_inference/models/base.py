5b1998df04d78930ce799523c1cfbfc0bb969a99
alex <apocthiel@gmail.com>
2018-11-10 16:58:04 -0800

    Merged

--- easy_inference/models/base.py
+++ easy_inference/models/base.py
@@ -22,7 +22,6 @@ class TensorflowBaseModel(BaseModel):
         if labels_path is not None:
             with open(labels_path, 'r') as f:
                 label_str = f.read()
-<<<<<<< HEAD
                 labels = json.loads(label_str)
                 labels = {int(key): value for key, value in labels.items()}
 
@@ -31,7 +30,3 @@ class TensorflowBaseModel(BaseModel):
                        *args, **kwargs)
         return cls(model_bytes=model_bytes,
                    *args, **kwargs)
-=======
-            return cls(model_bytes=model_bytes, labels_unparsed=label_str)
-        return cls(model_bytes=model_bytes)
->>>>>>> 483b30730ce7767c976915346e8f10c444ecef8a
