a635ebc588f4c1ed623f9e6f2242c15b59d74c8e
alex <apocthiel@gmail.com>
2018-12-08 13:16:02 -0800

    Upped version (trying to figure out dependency links on setup.py in another project...

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ scripts = []
 setup(
     name='easy_inference',
     scripts=scripts,
-    version='0.1',
+    version='0.2',
     description='This is a helper library for me to write simple easy_inference'
                 ' wrappers over pretrained TF models, that '
                 'require none of the original model definition code. ',
