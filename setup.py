b929c4a72a114eec5af0ab77dd139e569d07ff32
alex <apocthiel@gmail.com>
2018-12-08 13:01:44 -0800

    Tagged v0.1

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ from setuptools import setup
 
 scripts = []
 
+
 setup(
     name='easy_inference',
     scripts=scripts,
     version='0.1',
-    description='This is a helper library for me to write simple easy_inference wrappers over pretrained TF models, that '
+    description='This is a helper library for me to write simple easy_inference'
+                ' wrappers over pretrained TF models, that '
                 'require none of the original model definition code. ',
     packages=['easy_inference'],
     install_requires=["tensorflow-gpu",
